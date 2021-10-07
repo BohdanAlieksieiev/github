0e19bf55d0384d55f067657f6c52278040f94f60
BohdanAlieksieiev <bohdan.alieks@gmail.com>
2021-10-07 20:36:12 +0300

    Fix package

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import AppRouter from './router/AppRouter';
-import { Provider } from 'react-redux';
-import rootReducer from './store/rootReducer';
-import { createStore } from 'redux';
-
-const store = createStore(rootReducer);
 
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
+      <div>
         <AppRouter />
-      </Provider>
+      </div>
     </div>
   );
 }
